23ef575e68fc6e28d1a82d9a55d8ded4e1b9ca8e
jp <jpbs@cesar.school>
2024-11-10 22:54:00 -0300

    fix bug in option 5

--- python/main.py
+++ python/main.py
@@ -1,137 +1,102 @@
 import mysql.connector
 from tabulate import tabulate
 
-connection = mysql.connector.connect(
-    user = 'root', password = 'root', host = 'mysql', port = '3306', database = 'db')
-print('DB CONNECTED')
+def main():
+    connection = mysql.connector.connect(
+        user = 'root', password = 'root', host = 'mysql', port = '3306', database = 'db')
+    print('DB CONNECTED')
 
-#itens do armazem
-cursor = connection.cursor()
-cursor.execute('SELECT * FROM armazem')
-armazem = cursor.fetchall()
-print("TABELA ITENS:")
-#print(armazem)
-colunas = [desc[0] for desc in cursor.description]
-print(tabulate(armazem,headers= colunas,tablefmt="fancy_grid"))
+    cursor = connection.cursor()
+    
+    #itens do armazem
+    printar(cursor,"armazem")
 
-#funcionarios na empresa
-cursor.execute('SELECT * FROM funcionarios')
-funcionarios = cursor.fetchall()
-print("TABELA FUNCIONARIOS:")
-#print(armazem)
-colunas = [desc[0] for desc in cursor.description]
-print(tabulate(funcionarios,headers= colunas,tablefmt="fancy_grid"))
-
-
-#servicos
-cursor.execute('SELECT * FROM servicos')
-servicos = cursor.fetchall()
-print("TABELA SERVICOS:")
-#print(armazem)
-colunas = [desc[0] for desc in cursor.description]
-print(tabulate(servicos,headers= colunas,tablefmt="fancy_grid"))
-
-
-#FAZER O MENU
-while(1):
-    print("1-VER SERVICOS\n2-CRIAR SERVICOS\n3-FINALIZAR SERVICOS\n4-FUNCIONARIO\n5-ITEMS\n6-SAIR")
-    choice = int(input("Escolha: "))
-    match choice:
-        case 1:
-            cursor.execute('SELECT * FROM servicos')
-            s = cursor.fetchall()
-            colunas = [desc[0] for desc in cursor.description]
-            print(tabulate(s,headers= colunas,tablefmt="fancy_grid"))
-        case 2:
-            cliente = str(input("Nome Do cliente:"))
-            descricao = str(input("Descricao do Servico:"))
-            FuncionarioID = int(input("ID do funcionario:"))
-            ItemID = int(input("ID do material:"))
-            query = f'INSERT INTO servicos (cliente,descricao,FuncionarioID,ItemID) VALUES ("{cliente}","{descricao}",{FuncionarioID},{ItemID});'
-            cursor.execute(query)
-            connection.commit()
-            
-            # cursor.execute('SELECT * FROM servicos')
-            # s = cursor.fetchall()
-            # #print(s)
-
-            #botar funcionario assignado para 1
-            query = f'UPDATE funcionarios SET trabalhando = "1" WHERE ID = "{FuncionarioID}"'
-            cursor.execute(query)
-            connection.commit()
-        case 3:
-            id = int(input("ID do funcionario:"))
-            #atualizar funcionario para 0
-            query = f'UPDATE funcionarios SET trabalhando = "0" WHERE ID = {id};'
-            cursor.execute(query)
-            connection.commit()
-            
-            # cursor.execute('SELECT * FROM funcionarios')
-            # s = cursor.fetchall()
-            # print(s)
-
-            #remover servico
-            query = f'DELETE FROM servicos WHERE FuncionarioID = {id};'
-            cursor.execute(query)
-            connection.commit()
-
-            # cursor.execute("SELECT * FROM servicos")
-            # s = cursor.fetchall()
-            # print(s)
-        case 4:
-            choice = int(input("1-Ver Funcionarios\n2-Adicionar Funcionario\n"))
-            if (choice == 1):
-                cursor.execute('SELECT * FROM funcionarios')
-                s = cursor.fetchall()
-                colunas = [desc[0] for desc in cursor.description]
-                print(tabulate(s,headers= colunas,tablefmt="fancy_grid"))
-
-            elif (choice == 2):
-                nome_funcionario = str(input("Nome do Funcionario:"))
-                query = f'INSERT INTO funcionarios (nome,trabalhando) VALUES ("{nome_funcionario}","0");'
-                
-                cursor.execute(query)
-                connection.commit()
-
-                cursor.execute('SELECT * FROM funcionarios')
-                s = cursor.fetchall()
-                # print(s)
-                colunas = [desc[0] for desc in cursor.description]
-                print(tabulate(s,headers= colunas,tablefmt="fancy_grid"))
-        case 5:
-            cursor.execute('SELECT * FROM armazem')
-            s = cursor.fetchall()
-            colunas = [desc[0] for desc in cursor.description]
-            print(tabulate(s,headers= colunas,tablefmt="fancy_grid"))
-            choice = int(input("1-Abastecer Estoque\n2-Remover Estoque\n"))
-            
-            if (choice == 1):
-                #adicionar itens
-                item = str(input("Item a ser abastecido:"))
-                x = int(input("tamanho da remessa:"))
-                query = f'UPDATE armazem SET estoque = estoque + {x} WHERE ItemID = {item};'
-                
+    #funcionarios na empresa
+    printar(cursor,"funcionarios")
+    
+    #servicos
+    printar(cursor,"servicos")
+    
+    #FAZER O MENU
+    while(1):
+        print("1-VER SERVICOS\n2-CRIAR SERVICOS\n3-FINALIZAR SERVICOS\n4-FUNCIONARIOS\n5-ITEMS\n6-SAIR")
+        choice = int(input("Escolha: "))
+        match choice:
+            case 1:
+                printar(cursor,"servicos")
+            case 2:
+                cliente = str(input("Nome Do cliente:"))
+                descricao = str(input("Descricao do Servico:"))
+                FuncionarioID = int(input("ID do funcionario:"))
+                ItemID = int(input("ID do material:"))
+                query = f'INSERT INTO servicos (cliente,descricao,FuncionarioID,ItemID) VALUES ("{cliente}","{descricao}",{FuncionarioID},{ItemID});'
                 cursor.execute(query)
                 connection.commit()
                 
-                cursor.execute('SELECT * FROM armazem')
-                s = cursor.fetchall()
-                print(tabulate(s,headers= colunas,tablefmt="fancy_grid"))
-            #remover itens
-            elif (choice == 2):
-                item = str(input("Item a ser removido:"))
-                x = int(input("tamanho da remessa:"))
-                query = f'UPDATE armazem SET estoque = estoque - {x} WHERE ItemID = {item};'
-                
+
+                #botar funcionario assignado para 1
+                query = f'UPDATE funcionarios SET trabalhando = "1" WHERE ID = "{FuncionarioID}"'
                 cursor.execute(query)
                 connection.commit()
+            case 3:
+                id = int(input("ID do funcionario:"))
                 
-                cursor.execute('SELECT * FROM armazem')
-                s = cursor.fetchall()
-                print(tabulate(s,headers= colunas,tablefmt="fancy_grid"))
-        case 6:
-            break
+                #atualizar funcionario para 0
+                query = f'UPDATE funcionarios SET trabalhando = "0" WHERE ID = {id};'
+                cursor.execute(query)
+                connection.commit()
 
+                #remover servico
+                query = f'DELETE FROM servicos WHERE FuncionarioID = {id};'
+                cursor.execute(query)
+                connection.commit()
+            case 4:
+                choice = int(input("1-Ver Funcionarios\n2-Adicionar Funcionario\n"))
+                if (choice == 1):
+                    printar(cursor,"funcionarios")
 
+                elif (choice == 2):
+                    nome_funcionario = str(input("Nome do Funcionario:"))
+                    query = f'INSERT INTO funcionarios (nome,trabalhando) VALUES ("{nome_funcionario}","0");'
+                    
+                    cursor.execute(query)
+                    connection.commit()
 
-connection.close()
+                    printar(cursor,"funcionarios")
+            case 5:
+                printar(cursor,"armazem")
+                choice = int(input("1-Abastecer Estoque\n2-Remover Estoque\n"))
+                if (choice == 1):
+                    #adicionar itens
+                    item = str(input("ID do Item a ser abastecido:"))
+                    x = int(input("tamanho da remessa:"))
+                    query = f'UPDATE armazem SET estoque = estoque + {x} WHERE ItemID = {item};'
+                    
+                    cursor.execute(query)
+                    connection.commit()
+                    
+                    printar(cursor,"armazem")
+                #remover itens
+                elif (choice == 2):
+                    item = str(input("ID do Item a ser removido:"))
+                    x = int(input("tamanho da remessa:"))
+                    query = f'UPDATE armazem SET estoque = estoque - {x} WHERE ItemID = {item};'
+                    
+                    cursor.execute(query)
+                    connection.commit()
+                    
+                    printar(cursor,"armazem")
+            case 6:
+                break
+
+    connection.close()
+
+def printar(cursor,tabela):
+    cursor.execute(f'SELECT * FROM {tabela}')
+    armazem = cursor.fetchall()
+    print("TABELA ITENS:")
+    #print(armazem)
+    colunas = [desc[0] for desc in cursor.description]
+    print(tabulate(armazem,headers= colunas,tablefmt="fancy_grid"))
+
+main()
